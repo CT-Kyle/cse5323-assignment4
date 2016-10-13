b8eadaaeb4ca06a46be178ca7f7ab490e188a433
Caleb Kyle <ckyle@smu.edu>
2016-10-13 01:17:17 -0500

    Cleaned up some code from ICA stuff

--- ImageLab/ViewController.swift
+++ ImageLab/ViewController.swift
@@ -19,8 +19,7 @@ class ViewController: UIViewController   {
     let bridge = OpenCVBridgeSub()
     
     //MARK: Outlets in view
-    @IBOutlet weak var flashSlider: UISlider!
-    @IBOutlet weak var stageLabel: UILabel!
+
     
     //MARK: ViewController Hierarchy
     override func viewDidLoad() {
@@ -29,20 +28,17 @@ class ViewController: UIViewController   {
         self.view.backgroundColor = nil
         self.setupFilters()
         
-        self.bridge.loadHaarCascade(withFilename: "nose")
-        
         self.videoManager = VideoAnalgesic.sharedInstance
         self.videoManager.setCameraPosition(AVCaptureDevicePosition.front)
-        self.videoManager.setPreset("AVCaptureSessionPresetHigh")
         
         // create dictionary for face detection
         // HINT: you need to manipulate these proerties for better face detection efficiency
-        let optsDetector = [CIDetectorAccuracy:CIDetectorAccuracyLow,CIDetectorTracking:true] as [String : Any]
+        let optsDetector = [CIDetectorAccuracy:CIDetectorAccuracyHigh]
         
         // setup a face detector in swift
         self.detector = CIDetector(ofType: CIDetectorTypeFace,
                                   context: self.videoManager.getCIContext(), // perform on the GPU is possible
-                                  options: (optsDetector as [String : AnyObject]))
+                                  options: optsDetector)
         
         self.videoManager.setProcessingBlock(self.processImage)
         
@@ -50,44 +46,6 @@ class ViewController: UIViewController   {
             videoManager.start()
         }
     
-    }
-    
-    //MARK: Process image output
-    func processImage(_ inputImage:CIImage) -> CIImage{
-        
-        // detect faces
-        let f = getFaces(inputImage)
-        
-        // if no faces, just return original image
-        if f.count == 0 { return inputImage }
-        
-        var retImage = inputImage
-        
-        // if you just want to process on separate queue use this code
-        // this is a NON BLOCKING CALL, but any changes to the image in OpenCV cannot be displayed real time
-//        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0)) { () -> Void in
-//            self.bridge.setImage(retImage, withBounds: retImage.extent, andContext: self.videoManager.getCIContext())
-//            self.bridge.processImage()
-//        }
-        
-        // use this code if you are using OpenCV and want to overwrite the displayed image via OpenCv
-        // this is a BLOCKING CALL
-//        self.bridge.setTransforms(self.videoManager.transform)
-//        self.bridge.setImage(retImage, withBounds: retImage.extent, andContext: self.videoManager.getCIContext())
-//        self.bridge.processImage()
-//        retImage = self.bridge.getImage()
-        
-        //HINT: you can also send in the bounds of the face to ONLY process the face in OpenCV
-        // or any bounds to only process a certain bounding region in OpenCV
-        self.bridge.setTransforms(self.videoManager.transform)
-        self.bridge.setImage(retImage,
-                             withBounds: f[0].bounds, // the first face bounds
-                             andContext: self.videoManager.getCIContext())
-        
-        self.bridge.processImage()
-        retImage = self.bridge.getImageComposite() // get back opencv processed part of the image (overlayed on original)
-        
-        return retImage
     }
     
     //MARK: Setup filtering
@@ -124,55 +82,32 @@ class ViewController: UIViewController   {
     
     func getFaces(_ img:CIImage) -> [CIFaceFeature]{
         // this ungodly mess makes sure the image is the correct orientation
-        //let optsFace = [CIDetectorImageOrientation:self.videoManager.getImageOrientationFromUIOrientation(UIApplication.sharedApplication().statusBarOrientation)]
+
         let optsFace = [CIDetectorImageOrientation:self.videoManager.ciOrientation]
         // get Face Features
         return self.detector.features(in: img, options: optsFace) as! [CIFaceFeature]
         
     }
     
-    
-    
-    @IBAction func swipeRecognized(_ sender: UISwipeGestureRecognizer) {
-        switch sender.direction {
-        case UISwipeGestureRecognizerDirection.left:
-            self.bridge.processType += 1
-        case UISwipeGestureRecognizerDirection.right:
-            self.bridge.processType -= 1
-        default:
-            break
-            
-        }
+    func processImage(_ inputImage:CIImage) -> CIImage{
         
-        stageLabel.text = "Stage: \(self.bridge.processType)"
-
+        // detect faces
+        let f = getFaces(inputImage)
+        
+        // if no faces, just return original image
+        if f.count == 0 { return inputImage }
+        
+        //otherwise apply the filters to the faces
+        return applyFiltersToFaces(inputImage, features: f)
     }
     
-    //MARK: Convenience Methods for UI Flash and Camera Toggle
-    @IBAction func flash(_ sender: AnyObject) {
-        if(self.videoManager.toggleFlash()){
-            self.flashSlider.value = 1.0
-        }
-        else{
-            self.flashSlider.value = 0.0
-        }
-    }
     
+//    //MARK: Convenience Methods for UI Flash and Camera Toggle
+ 
     @IBAction func switchCamera(_ sender: AnyObject) {
-        self.videoManager.setFPS(desiredFrameRate: 5.0)
-
         self.videoManager.toggleCameraPosition()
     }
-    
-    @IBAction func setFlashLevel(_ sender: UISlider) {
-        if(sender.value>0.0){
-            self.videoManager.turnOnFlashwithLevel(sender.value)
-        }
-        else if(sender.value==0.0){
-            self.videoManager.turnOffFlash()
-        }
-    }
 
-   
+
 }
 
